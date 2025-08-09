6995c8c92a06ad0f3fb5d89c0810808b5dee414f
... <...>
2025-08-09 10:43:23 +0000

    fb login success (need https and port 8080 and fb developer allow localhost). google login success (redirect uri is https://localhost:8080/auth/page/google/callback)

--- app/devel.hs
+++ app/devel.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE PackageImports #-}
+import Prelude              (IO, ($), putStrLn)
 import "haskellers" Application (getApplicationDev)
 import Network.Wai.Handler.Warp
     (runSettings, defaultSettings, setPort)
+import Network.Wai.Handler.WarpTLS
+  (tlsSettings, runTLS)
 import Control.Concurrent (forkIO)
 import System.Directory (doesFileExist, removeFile)
 import System.Exit (exitSuccess)
@@ -9,9 +12,11 @@ import Control.Concurrent (threadDelay)
 
 main :: IO ()
 main = do
+    let tls = tlsSettings "./cert.pem" "./key.pem"
     putStrLn "Starting devel application"
     (port, app) <- getApplicationDev
-    forkIO $ runSettings (setPort port defaultSettings) app
+    -- forkIO $ runSettings (setPort port defaultSettings) app
+    forkIO $ runTLS tls (setPort port defaultSettings) app
     loop
 
 loop :: IO ()
--- issue.md
+++ issue.md
@@ -1,4 +1,7 @@
--[x] [the Unix socket directories couldn't match between pg_ctl and psql](https://discourse.nixos.org/t/set-up-a-simple-postgres-instance/60280/2?u=dycan)
--[x] [Starting postgresql demands that /run/postgresql exists](https://github.com/NixOS/nixpkgs/issues/83770)
+- [x] [the Unix socket directories couldn't match between pg_ctl and psql](https://discourse.nixos.org/t/set-up-a-simple-postgres-instance/60280/2?u=dycan)
+- [x] [Starting postgresql demands that /run/postgresql exists](https://github.com/NixOS/nixpkgs/issues/83770)
 
--[] 似乎是沒有用https所以facebook login不行
+- [x] 似乎是沒有用https所以facebook login不行
+      A: 用 warp-tls, devel.hs, port 8080 即可。
+
+- [x] Facebook Developer的應用程式網域要用localhost，不能用127.0.0.1。Facebook Login至此成功了。
